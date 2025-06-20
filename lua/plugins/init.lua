6a05ef33ee6df8e87354d614f7e82a84914d16f4
Jerome Portier <jeportie@f1br8s11.clusters.42paris.fr>
2025-06-20 16:26:09 +0200

    pause

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,4 +13,49 @@ return {
   {
     require 'plugins.telescope',
   },
+  {
+    require 'plugins.lazydev',
+  },
+  {
+    require 'plugins.lspconfig',
+  },
+  {
+    require 'plugins.autoformat',
+  },
+  {
+    require 'plugins.autocompletion',
+  },
+  {
+    require 'plugins.tokyonight',
+  },
+  {
+    require 'plugins.todocomments',
+  },
+  {
+    require 'plugins.mini',
+  },
+  {
+    require 'plugins.treesitter',
+  },
+  -- {
+  --   ui = {
+  --     -- If you are using a Nerd Font: set icons to an empty table which will use the
+  --     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+  --     icons = vim.g.have_nerd_font and {} or {
+  --       cmd = '⌘',
+  --       config = '🛠',
+  --       event = '📅',
+  --       ft = '📂',
+  --       init = '⚙',
+  --       keys = '🗝',
+  --       plugin = '🔌',
+  --       runtime = '💻',
+  --       require = '🌙',
+  --       source = '📄',
+  --       start = '🚀',
+  --       task = '📌',
+  --       lazy = '💤 ',
+  --     },
+  --   },
+  -- },
 }
