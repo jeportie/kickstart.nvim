acf278be838ca751608155229c15e3f999e03589
jeromeDev94 <jeromep.dev@gmail.com>
2025-06-24 18:24:15 +0200

    push pause

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,50 +1,34 @@
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      {
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-        config = function(_, opts)
-          require("luasnip").config.set_config(opts)
-          require "plugin_config.luasnip"
-        end,
-      },
-
-      -- autopairing of (){}[] etc
-      {
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
-        },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
-
-          -- setup cmp for autopairs
-          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
-      },
-
-      -- cmp sources plugins
-      {
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "https://codeberg.org/FelipeLema/cmp-async-path.git",
-	    "hrsh7th/cmp-cmdline",               -- cmdline source
-	    "hrsh7th/cmp-calc",                  -- calculator source
-	    "hrsh7th/cmp-emoji",                 -- emoji source
-	    "uga-rosa/cmp-dictionary",           -- dictionary source
-	    "hrsh7th/cmp-nvim-lsp-signature-help", -- signature help
-	    "hrsh7th/cmp-omni",                  -- omnifunc source
-      },
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    {
+      -- snippet plugin
+      'L3MON4D3/LuaSnip',
+      dependencies = 'rafamadriz/friendly-snippets',
+      opts = { history = true, updateevents = 'TextChanged,TextChangedI' },
+      config = function(_, opts)
+        require('luasnip').config.set_config(opts)
+        require 'plugin_config.luasnip'
+      end,
     },
-    opts = function()
-      return require "plugin_config.cmp"
-    end,
-  }
+
+    -- cmp sources plugins
+    {
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'https://codeberg.org/FelipeLema/cmp-async-path.git',
+      'hrsh7th/cmp-cmdline', -- cmdline source
+      'hrsh7th/cmp-calc', -- calculator source
+      'hrsh7th/cmp-emoji', -- emoji source
+      'uga-rosa/cmp-dictionary', -- dictionary source
+      'hrsh7th/cmp-nvim-lsp-signature-help', -- signature help
+      'hrsh7th/cmp-omni', -- omnifunc source
+    },
+  },
+  opts = function()
+    return require 'plugin_config.cmp'
+  end,
+}
