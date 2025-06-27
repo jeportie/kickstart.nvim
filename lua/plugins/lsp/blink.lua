15b545f36cc1335195a01fb8d63e4bacbb9b7cab
Jerome Portier <jeportie@f1br8s14.clusters.42paris.fr>
2025-06-27 13:31:01 +0200

    push pause

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -1,72 +1,89 @@
-return { -- Autocompletion
-  'saghen/blink.cmp',
-  event = 'VimEnter',
-  version = '1.*',
-  dependencies = {
-    'folke/lazydev.nvim',
+-- -------------------------------------------------------------------------- --
+--                                                                            --
+--                                                        :::      ::::::::   --
+--   blink.lua                                          :+:      :+:    :+:   --
+--                                                    +:+ +:+         +:+     --
+--   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        --
+--                                                +#+#+#+#+#+   +#+           --
+--   Created: 2025/06/27 10:56:27 by jeportie          #+#    #+#             --
+--   Updated: 2025/06/27 12:00:59 by jeportie         ###   ########.fr       --
+--                                                                            --
+-- -------------------------------------------------------------------------- --
+
+return {
+  {
+    'saghen/blink.compat',
+    version = '2.*',
+    lazy = true,
+    opts = {},
   },
-  --- @module 'blink.cmp'
-  --- @type blink.cmp.Config
-  opts = {
-    keymap = {
-      -- 'default' (recommended) for mappings similar to built-in completions
-      --   <c-y> to accept ([y]es) the completion.
-      --    This will auto-import if your LSP supports it.
-      --    This will expand snippets if the LSP sent a snippet.
-      -- 'super-tab' for tab to accept
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- For an understanding of why the 'default' preset is recommended,
-      -- you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      --
-      -- All presets have the following mappings:
-      -- <tab>/<s-tab>: move to right/left of your snippet expansion
-      -- <c-space>: Open menu or open docs if already open
-      -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'default',
-
-      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+  {
+    'saghen/blink.cmp',
+    version = '1.*',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-calc',
+      'hrsh7th/cmp-emoji',
+      'uga-rosa/cmp-dictionary',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-omni',
     },
-
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
-    },
-
-    completion = {
-      -- By default, you may press `<c-space>` to show the documentation.
-      -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
-    },
-
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
-      providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+    opts = {
+      keymap = { preset = 'enter' },
+      appearance = { nerd_font_variant = 'mono' },
+      completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
       },
+      sources = {
+        default = {
+          'lazydev',
+          'lsp',
+          'path',
+          'snippets',
+          'buffer',
+          'nvim_lua',
+          'nvim_lsp',
+          'buffer',
+          'path',
+          'cmdline',
+          'calc',
+          'emoji',
+          'dictionary',
+          'omni',
+        },
+
+        keymap = {
+          -- scroll documentation
+          ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+          ['<C-e>'] = { 'hide' },
+          -- arrow keys, same as <C-p>/<C-n>
+          ['<Up>'] = { 'select_prev', 'fallback' },
+          ['<Down>'] = { 'select_next', 'fallback' },
+        },
+        providers = {
+          lazydev = {
+            name = 'LazyDev', -- display name in the menu
+            module = 'lazydev.integrations.blink', -- the actual Lua module to require
+            score_offset = 100,
+          },
+          nvim_lua = { name = 'nvim_lua', module = 'blink.compat.source' },
+          nvim_lsp = { name = 'nvim_lsp', module = 'blink.compat.source' },
+          buffer = { name = 'buffer', module = 'blink.compat.source' },
+          path = { name = 'path', module = 'blink.compat.source' },
+          cmdline = { name = 'cmdline', module = 'blink.compat.source' },
+          calc = { name = 'calc', module = 'blink.compat.source' },
+          emoji = { name = 'emoji', module = 'blink.compat.source' },
+          dictionary = { name = 'dictionary', module = 'blink.compat.source' },
+          omni = { name = 'omni', module = 'blink.compat.source' },
+        },
+      },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+      snippets = { preset = 'luasnip' },
     },
-
-    snippets = { preset = 'luasnip' },
-
-    -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-    -- which automatically downloads a prebuilt binary when enabled.
-    --
-    -- By default, we use the Lua implementation instead, but you may enable
-    -- the rust implementation via `'prefer_rust_with_warning'`
-    --
-    -- See :h blink-cmp-config-fuzzy for more information
-    fuzzy = { implementation = 'lua' },
-
-    -- Shows a signature help window while you type arguments for a function
-    signature = { enabled = true },
   },
 }
