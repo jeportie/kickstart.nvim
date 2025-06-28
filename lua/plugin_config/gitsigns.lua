7d8536b8b510a1318cfe1e30b3bced0c8e48d953
Jerome Portier <jeportie@f1br8s20.clusters.42paris.fr>
2025-06-28 12:20:15 +0200

    push pause

--- lua/plugin_config/gitsigns.lua
+++ lua/plugin_config/gitsigns.lua
@@ -1,8 +1,8 @@
-dofile(vim.g.base46_cache .. "git")
+dofile(vim.g.base46_cache .. 'git')
 
 return {
   signs = {
-    delete = { text = "󰍵" },
-    changedelete = { text = "󱕖" },
+    delete = { text = '󰍵' },
+    changedelete = { text = '󱕖' },
   },
 }
--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -1,18 +1,24 @@
 return {
   'nvimdev/lspsaga.nvim',
-  event = 'LspAttach', -- loads when an LSP client attaches (requires lazy.nvim 2023‑July‑9 or later)
-  ft = { 'c', 'cpp', 'lua', 'rust', 'go' }, -- loads when one of these filetypes is opened
   dependencies = {
-    'nvim-tree/nvim-web-devicons', -- optional, for icons in UI
-    'neovim/nvim-lspconfig', -- ensures lspsaga is a dependency of your LSP setup
-    'nvim-treesitter/nvim-treesitter', -- optional, for better UI integration
+    'nvim-tree/nvim-web-devicons',
+    'neovim/nvim-lspconfig',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  keys = {
+    { '<leader>fp', '<Cmd>Lspsaga peek_definition<CR>', desc = '[f]loat [p]eek def' },
+    { '<leader>fi', '<Cmd>Lspsaga incoming_calls<CR>', desc = '[f]loat [i]ncoming' },
+    { '<leader>fo', '<Cmd>Lspsaga outgoing_calls<CR>', desc = '[f]loat [o]utgoing' },
+    { '<leader>ca', '<Cmd>Lspsaga code_action<CR>', desc = '[c]ode [a]ction' },
+    -- you can also add your diag keys here to lazy-load on those presses:
+    { '<CR>', '<Cmd>Lspsaga show_cursor_diagnostics<CR>', desc = '[s]how [c]ursor diag' },
+    { '<leader>sl', '<Cmd>Lspsaga show_line_diagnostics<CR>', desc = '[s]how [l]ine diag' },
+    { '<leader>sb', '<Cmd>Lspsaga show_buf_diagnostics<CR>', desc = '[s]how [b]uffer diag' },
+    { '<leader>sw', '<Cmd>Lspsaga show_workspace_diagnostics<CR>', desc = '[s]how [w]orkspace diags' },
+    { '[e', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', desc = 'prev [e]rror' },
+    { ']e', '<Cmd>Lspsaga diagnostic_jump_next<CR>', desc = 'next [e]rror' },
   },
   config = function()
-    require('lspsaga').setup {
-      -- Your lspsaga configuration options go here.
-    }
-    -- 2. Add your buffer-local keymap for peek_definition
-    --    We're using vim.keymap.set; if you have a 'map' wrapper, swap this out accordingly.
-    vim.keymap.set('n', '<leader>dp', '<Cmd>Lspsaga peek_definition<CR>', { buffer = true, silent = true })
+    require('lspsaga').setup {}
   end,
 }
