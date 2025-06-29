f45493525c53c9fb88b1fe9b3263c038a6c54695
Jerome Portier <jeportie@f1br6s20.clusters.42paris.fr>
2025-06-29 16:52:10 +0200

    push pause

--- lua/plugins/custom/header.lua
+++ lua/plugins/custom/header.lua
@@ -3,6 +3,7 @@ return {
   cmd = { 'Stdheader' },
   keys = { '<F1>' },
   opts = {
+    school = false,
     default_map = true, -- Default mapping <F1> in normal mode.
     auto_update = true, -- Update header when saving.
     user = 'jeportie', -- Your user.
