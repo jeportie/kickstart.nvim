66124f4de666819e4d8cdc6ff356c0f8d0355930
Jerome Portier <jeportie@f1br6s20.clusters.42paris.fr>
2025-06-29 15:00:12 +0200

    push pause

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -8,4 +8,27 @@ M.base46 = {
   transparency = false,
 }
 
+M.ui = {
+  telescope = { style = 'bordered' }, -- borderless / bordered
+  statusline = {
+    theme = 'default',
+    -- theme = 'vscode',
+    -- theme = 'vscode_colored',
+    -- theme = 'minimal',
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = 'default',
+    order = nil,
+    modules = nil,
+  },
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { 'treeOffset', 'buffers', 'tabs', 'btns' },
+    modules = nil,
+    bufwidth = 21,
+  },
+}
+
 return M
