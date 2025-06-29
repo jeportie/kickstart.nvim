ba65ea701cb4364ba45ae646517b8449e1a7121e
Jerome Portier <jeportie@f1br6s20.clusters.42paris.fr>
2025-06-29 17:50:57 +0200

    push pause

--- lua/plugins/ui/todocomments.lua
+++ lua/plugins/ui/todocomments.lua
@@ -1,2 +1,13 @@
 -- Highlight todo, notes, etc in comments
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  optional = true,
+  -- stylua: ignore
+  keys = {
+    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+    { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+  },
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false },
+}
