b62ce059164a61c020691115b3fc4e8c72c6b5b7
jeromep <jeromep.dev@gmail.com>
2025-09-25 18:05:16 +0200

    push pause

--- lua/plugins/tools/rest.lua
+++ lua/plugins/tools/rest.lua
@@ -1,31 +1,21 @@
 return {
   'rest-nvim/rest.nvim',
+  build = false, -- 🚫 disables any build/install step
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-telescope/telescope.nvim',
-
-    -- required deps
-    { 'nvim-neotest/nvim-nio' },
-    { 'lunarmodules/lua-mimetypes', name = 'mimetypes' }, -- ✅ confirmed Lua repo
-    { 'manoelcampos/xml2lua' },
-    { 'j-hui/fidget.nvim' },
-
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, 'http')
-    end,
   },
   config = function()
     require('rest-nvim').setup {
       rocks = {
-        enabled = false, -- stop luarocks completely
+        enabled = false,
+        hererocks = false,
       },
       env = {
         enable = true,
         pattern = '.*%.env.*',
       },
     }
-
     pcall(require('telescope').load_extension, 'rest')
   end,
 }
