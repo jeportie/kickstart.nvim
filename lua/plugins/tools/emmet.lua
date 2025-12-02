2485d54e3d24791fcf9fdc155a45171efb474b66
jeromep <jeromep.dev@gmail.com>
2025-12-02 14:48:59 +0100

    push pause

--- lua/plugins/tools/emmet.lua
+++ lua/plugins/tools/emmet.lua
@@ -1,8 +1,24 @@
 return {
   'mattn/emmet-vim',
-  lazy = false, -- 🚨 load immediately
+  ft = { 'html', 'css', 'javascriptreact', 'typescriptreact', 'jsx', 'tsx' },
+  init = function()
+    vim.g.user_emmet_install_global = 0
+    vim.g.user_emmet_mode = 'a'
+    vim.g.user_emmet_leader_key = '<C-y>'
+  end,
   config = function()
-    vim.g.user_emmet_leader_key = '<C-e>'
-    vim.g.user_emmet_mode = 'inv'
+    -- FORCELOAD EMMET AUTOLOAD (fix NVChad issue)
+    vim.cmd 'runtime! autoload/emmet.vim'
+
+    vim.cmd [[
+      autocmd FileType html,css,javascriptreact,typescriptreact,jsx,tsx EmmetInstall
+    ]]
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'html', 'css', 'javascriptreact', 'typescriptreact', 'jsx', 'tsx' },
+      callback = function()
+        vim.bo.omnifunc = 'emmet#completeTag'
+      end,
+    })
   end,
 }
