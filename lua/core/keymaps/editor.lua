347f9f3f0908a1ed9196a224b90ed67f80d3e8f4
Jérôme Portier <jeportie@MacBook-Air.local>
2025-12-28 21:57:23 +0100

    added new keys in editor

--- lua/core/keymaps/editor.lua
+++ lua/core/keymaps/editor.lua
@@ -1,131 +1,136 @@
 local map = vim.keymap.set
-local global = vim.g
-
-local vimKeymap = vim.keymap.set
-local nvimKeymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local g = vim.g
 
 -- Leader keys
-global.mapleader = ' '
-global.maplocalleader = ','
+g.mapleader = ' '
+g.maplocalleader = ','
 
+-- ============================================================================
 -- General
+-- ============================================================================
+
 map('n', ';', ':', { desc = 'Enter command mode' })
-
-map('i', 'jk', '<ESC>', { desc = 'Exit insert mode' })
-
+map('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
 map('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
 
-map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-map('n', '<Left>', '<C-w>h', { desc = 'Move focus to the left window' })
-map('n', '<Right>', '<C-w>l', { desc = 'Move focus to the right window' })
-map('n', '<Down>', '<C-w>j', { desc = 'Move focus to the lower window' })
-map('n', '<Up>', '<C-w>k', { desc = 'Move focus to the upper window' })
-
 map('n', '<C-s>', '<cmd>w<CR>', { desc = 'Save file' })
+map('n', '<C-c>', '<cmd>%y+<CR>', { desc = 'Copy whole file' })
 
-map('n', '<C-c>', '<cmd>%y+<CR>', { desc = 'Copy file' })
+-- ============================================================================
+-- Window navigation
+-- ============================================================================
 
--- Scrolling and centering
-opts.desc = 'Automatically centers when C-d'
-nvimKeymap('n', '<C-d>', '<C-d>zz', opts)
+map('n', '<C-h>', '<C-w>h', { desc = 'Focus left window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Focus right window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Focus lower window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Focus upper window' })
 
-opts.desc = 'Automatically centers when C-u'
-nvimKeymap('n', '<C-u>', '<C-u>zz', opts)
+map('n', '<Left>', '<C-w>h', { desc = 'Focus left window' })
+map('n', '<Right>', '<C-w>l', { desc = 'Focus right window' })
+map('n', '<Down>', '<C-w>j', { desc = 'Focus lower window' })
+map('n', '<Up>', '<C-w>k', { desc = 'Focus upper window' })
 
-opts.desc = 'Cursor follows downscoll'
-vim.keymap.set('n', '<C-e>', '<C-e>j', opts)
+-- ============================================================================
+-- Scrolling & centering
+-- ============================================================================
 
-opts.desc = 'Cursor follows upscoll'
-vim.keymap.set('n', '<C-y>', '<C-y>k', opts)
+map('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
+map('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
 
-opts.desc = 'Center when searching'
-nvimKeymap('n', 'n', 'nzzzv', opts)
-nvimKeymap('n', 'N', 'Nzzzv', opts)
+map('n', '<C-e>', '<C-e>j', { desc = 'Scroll down with cursor follow' })
+map('n', '<C-y>', '<C-y>k', { desc = 'Scroll up with cursor follow' })
 
--- Search and replace
-opts.desc = 'Select word (\\v<..>)'
-vimKeymap('n', '<leader>rs', '/\\v<><Left>', { desc = opts.desc })
+map('n', 'n', 'nzzzv', { desc = 'Next search result centered' })
+map('n', 'N', 'Nzzzv', { desc = 'Previous search result centered' })
 
-nvimKeymap('n', '<leader>rw', [[:let @/='\<'.expand('<cword>').'\>'<CR>:%s/<C-r>///g<Left><Left>]], { desc = 'Rename word under cursor' })
+-- ============================================================================
+-- Search & replace
+-- ============================================================================
 
+map('n', '<leader>rs', '/\\v<><Left>', {
+  desc = 'Search word boundary (\\v< >)',
+})
+
+map('n', '<leader>rw', function()
+  vim.cmd [[
+    let @/='\<'.expand('<cword>').'\>'
+    %s/<C-r>//g
+  ]]
+end, { desc = 'Rename word under cursor' })
+
+-- ============================================================================
 -- Insert mode navigation
-opts.desc = 'Move up'
-nvimKeymap('i', '<C-k>', '<Up>', opts)
+-- ============================================================================
 
-opts.desc = 'Move down'
-nvimKeymap('i', '<C-j>', '<Down>', opts)
+map('i', '<C-h>', '<Left>', { desc = 'Move left' })
+map('i', '<C-l>', '<Right>', { desc = 'Move right' })
+map('i', '<C-j>', '<Down>', { desc = 'Move down' })
+map('i', '<C-k>', '<Up>', { desc = 'Move up' })
 
-opts.desc = 'Move left'
-nvimKeymap('i', '<C-h>', '<Left>', opts)
-
-opts.desc = 'Move right'
-nvimKeymap('i', '<C-l>', '<Right>', opts)
-
+-- ============================================================================
 -- Visual mode
-opts.desc = 'Move selection one line down'
-nvimKeymap('v', 'J', ":m '>+1<CR>gv=gv", opts)
+-- ============================================================================
 
-opts.desc = 'Move selection one line up'
-nvimKeymap('v', 'K', ":m '<-2<CR>gv=gv", opts)
+map('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+map('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 
-opts.desc = 'Indentation plus'
-nvimKeymap('v', '>', '>gv', opts)
+map('v', '>', '>gv', { desc = 'Indent right' })
+map('v', '<', '<gv', { desc = 'Indent left' })
 
-opts.desc = 'Indentation minus'
-nvimKeymap('v', '<', '<gv', opts)
+map('v', 'p', '"_dP', { desc = 'Paste without yanking' })
 
-opts.desc = 'Keep last yanked when pasting'
-nvimKeymap('v', 'p', '"_dP', opts)
+-- ============================================================================
+-- Tab width switcher
+-- ============================================================================
 
--- Tab stop switcher
-local function switchTabStop()
-  if vim.bo.tabstop == 4 then
-    vim.bo.tabstop = 2
-    vim.bo.shiftwidth = 2
-    vim.bo.softtabstop = 2
-  else
-    vim.bo.tabstop = 4
-    vim.bo.shiftwidth = 4
-    vim.bo.softtabstop = 4
-  end
-end
+map('n', '<leader>ts', function()
+  local size = vim.bo.tabstop == 4 and 2 or 4
+  vim.bo.tabstop = size
+  vim.bo.shiftwidth = size
+  vim.bo.softtabstop = size
+end, { desc = 'Toggle tab width (2 / 4)' })
 
-vimKeymap('n', '<leader>ts', switchTabStop, { desc = 'Switch tab stop' })
-
+-- ============================================================================
 -- UI toggles
+-- ============================================================================
+
 map('n', '<leader>ur', '<cmd>set rnu!<CR>', { desc = 'Toggle relative numbers' })
 
 map('n', '<leader>uc', function()
   if vim.wo.colorcolumn:find '80' then
-    vim.cmd 'set colorcolumn-=80'
+    vim.cmd 'set colorcolumn-='
   else
     vim.cmd 'set colorcolumn=80'
   end
-end, { desc = 'Toggle colorcolumn (80)' })
+end, { desc = 'Toggle colorcolumn at 80' })
 
--- Comments
-map('n', '<leader>/', '<cmd>normal gcc<CR>', {
-  desc = 'Toggle comment line',
-})
+-- ============================================================================
+-- Comments (Comment.nvim compatible)
+-- ============================================================================
 
-map('v', '<leader>/', '<cmd>normal gc<CR>', {
-  desc = 'Toggle comment selection',
-})
+map('n', '<leader>/', 'gcc', { desc = 'Toggle comment line', remap = true })
+map('v', '<leader>/', 'gc', { desc = 'Toggle comment selection', remap = true })
 
--- NvChad
+-- ============================================================================
+-- NvChad / UI
+-- ============================================================================
+
 map('n', '<leader>nth', function()
   require('nvchad.themes').open()
-end, { desc = 'telescope nvchad themes' })
-map('n', '<leader>ch', '<cmd>NvCheatsheet<CR>', { desc = 'NVChad Cheatsheet' })
+end, { desc = 'Open NvChad themes' })
 
+map('n', '<leader>ch', '<cmd>NvCheatsheet<CR>', {
+  desc = 'NvChad cheatsheet',
+})
+
+-- ============================================================================
 -- WhichKey
-map('n', '<leader>wK', '<cmd>WhichKey <CR>', { desc = 'whichkey all keymaps' })
+-- ============================================================================
+
+map('n', '<leader>wK', '<cmd>WhichKey<CR>', {
+  desc = 'WhichKey: all mappings',
+})
 
 map('n', '<leader>wk', function()
   vim.cmd('WhichKey ' .. vim.fn.input 'WhichKey: ')
-end, { desc = 'whichkey query lookup' })
+end, { desc = 'WhichKey: query' })
