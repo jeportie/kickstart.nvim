44d7d4246f3220bf3979289238c5494fa170af32
Jerome Portier <jeromep.dev@gmail.com>
2026-01-04 23:06:01 +0100

    push pause

--- lua/plugins/tools/blink.lua
+++ lua/plugins/tools/blink.lua
@@ -1,45 +1,8 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    completion = {
-      list = {
-        selection = {
-          preselect = true,
-          auto_insert = false,
-        },
-      },
-    },
-
-    sorting = {
-      priority_weight = 2,
-      comparators = {
-        "exact",
-        "score",
-
-        function(a, b)
-          local priority = {
-            Function = 1,
-            Method = 1,
-            Constructor = 1,
-
-            Snippet = 2,
-
-            Keyword = 3,
-            Variable = 4,
-            Text = 5,
-          }
-
-          local ak = priority[a.kind_name] or 100
-          local bk = priority[b.kind_name] or 100
-
-          if ak ~= bk then
-            return ak < bk
-          end
-        end,
-
-        "length",
-        "order",
-      },
+    sources = {
+      default = { "snippets", "lsp", "path", "buffer" },
     },
   },
 }
