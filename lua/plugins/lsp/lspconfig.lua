57d492ecb855c8daa363a9421f93578b63114258
Jerome Portier <jeromep.dev@gmail.com>
2026-01-05 00:42:33 +0100

    fixed lsp by removing typescript tools and keeping lazy nvim extension + fixed snippets in cmp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,11 +1,6 @@
 return {
   'neovim/nvim-lspconfig',
   opts = function(_, opts)
-    local lsp_keys = require('config.keymaps.lsp')
-
-    opts.on_attach = function(client, bufnr)
-      lsp_keys.on_attach(client, bufnr)
-    end
     -- declare non-TS servers here
     opts.servers = opts.servers or {}
     opts.servers.lua_ls = {}
--- lua/plugins/tools/blink.lua
+++ lua/plugins/tools/blink.lua
@@ -1,8 +1,26 @@
 return {
-  "saghen/blink.cmp",
-  opts = {
-    sources = {
-      default = { "snippets", "lsp", "path", "buffer" },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "snippets", "lsp", "path", "buffer" },
+        providers = {
+          snippets = {
+            name = "snippets",
+            module = "blink.cmp.sources.snippets",
+            score_offset = 80, -- big bump
+          },
+          lsp = {
+            score_offset = 0,
+          },
+          buffer = {
+            score_offset = -20,
+          },
+          path = {
+            score_offset = -10,
+          },
+        },
+      },
     },
   },
 }
