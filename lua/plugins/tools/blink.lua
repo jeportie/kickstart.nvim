8bce5c7e9dcadd2064fe846899a0fc0c6acff027
deepvdb <jeromevdb.production@gmail.com>
2026-01-07 15:45:37 +0100

    push pause

--- lua/plugins/tools/blink.lua
+++ lua/plugins/tools/blink.lua
@@ -2,16 +2,22 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
+      completion = {
+        ghost_text = {
+          enabled = false, -- ❌ disable inline grey text
+        },
+      },
       sources = {
-        default = { "snippets", "lsp", "path", "buffer" },
+        default = { "lsp", "snippets", "path", "buffer" },
         providers = {
+          lsp = {
+            score_offset = 100,
+            trigger_characters = { "." },
+          },
           snippets = {
             name = "snippets",
             module = "blink.cmp.sources.snippets",
-            score_offset = 80, -- big bump
-          },
-          lsp = {
-            score_offset = 0,
+            score_offset = 10, -- big bump
           },
           buffer = {
             score_offset = -20,
